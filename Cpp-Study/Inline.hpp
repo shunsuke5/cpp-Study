acbadc36e4356350a90873ea8df9628493f8b8a4
shunsuke5 <shunsuke1359@gmail.com>
2025-05-28 22:55:00 +0900

    インライン関数

---
+++ Cpp-Study/Inline.hpp
@@ -0,0 +1,12 @@
+#ifndef INLINE_H_
+#define INLINE_H_
+#include <iostream>
+
+class Inline {
+public:
+    void Print() {
+        std::cout << "インラインメンバ関数" << std::endl;
+    }
+};
+
+#endif // !INLINE_H_
