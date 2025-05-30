f6e9c981b98c9949db35a69f2d4294b96663f5ed
shunsuke5 <shunsuke1359@gmail.com>
2025-05-30 14:03:08 +0900

    仮想継承

--- Cpp-Study/main.cpp
+++ Cpp-Study/main.cpp
@@ -9,10 +9,41 @@
 #define ARRAY_SIZE(array) (sizeof (array) / sizeof *(array))
 using namespace std;
 
-int main() {
-    int n[100];
-    cout << ARRAY_SIZE(n) << endl;
+class ABase {
+public:
+    ABase(int x) : m_x(x) {}
+    virtual ~ABase() {}
 
-    IntArray array(5);
-    array.Get(6);
+public:
+    void Show() {
+        cout << m_x << endl;
+    }
+
+private:
+    int m_x;
+};
+
+class A1 : virtual public ABase {
+public:
+    A1() : ABase(1) {}
+};
+
+class A2 : virtual public ABase {
+public:
+    A2() : ABase(2) {}
+};
+
+class C : public A1, public A2 {
+public:
+    C() : ABase(3) {}
+};
+
+int main() {
+    A1 a1;
+    A2 a2;
+    C c;
+
+    a1.Show();  // 1
+    a2.Show();  // 2
+    c.Show();   // 3
 }
