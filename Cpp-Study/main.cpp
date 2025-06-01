b13c8205b6e04c621a973b609b289ba56544ea68
shunsuke5 <shunsuke1359@gmail.com>
2025-06-01 19:53:00 +0900

    メンバ関数へのポインタ

--- Cpp-Study/main.cpp
+++ Cpp-Study/main.cpp
@@ -12,28 +12,8 @@
 #define ARRAY_SIZE(array) (sizeof (array) / sizeof *(array))
 using namespace std;
 
-int Add(int a, int b) { return a + b; }
-int Sub(int a, int b) { return a - b; }
-int Mul(int a, int b) { return a * b; }
-int Div(int a, int b) { return a / b; }
-
-/*
-* 関数ポインタ
-*/
-int (* const ADD) (int a, int b) = { Add };
-
-/*
-* 関数ポインタの配列
-*/
-int (* const FP_OPERATOR[]) (int a, int b) = {
-    Add, Sub, Mul, Div
-};
-
-const char* const OPERATION_NAME[] = {
-    "加算", "減算", "乗算", "除算" 
-};
-
-class Calculator {
+class Calculator
+{
 public:
     void Run();
 
@@ -42,36 +22,59 @@ private:
     void Calculate();
 
 private:
+    int Add() { return m_a + m_b; }
+    int Sub() { return m_a - m_b; }
+    int Mul() { return m_a * m_b; }
+    int Div() { return m_a / m_b; }
+
+    typedef int (Calculator::*FpOp2) ();
+    static const FpOp2 FP_OPERATOR[];
+
+    static const char* const OPERATION_NAME[];
+
+private:
     int m_a, m_b;
 };
 
-void Calculator::Run() {
+const Calculator::FpOp2 Calculator::FP_OPERATOR[] =
+{
+    &Calculator::Add, &Calculator::Sub,
+    &Calculator::Mul, &Calculator::Div
+};
+
+const char* const Calculator::OPERATION_NAME[] =
+{
+    "加算", "減算", "乗算", "除算"
+};
+
+void Calculator::Run()
+{
     while (Input()) {
         Calculate();
     }
 }
 
-bool Calculator::Input() {
+bool Calculator::Input()
+{
     cout << "2つの値を入力してください > " << flush;
     m_b = 0;
     cin >> m_a >> m_b;
     return m_b != 0;
 }
 
-void Calculator::Calculate() {
+void Calculator::Calculate()
+{
     static const size_t SIZE = ARRAY_SIZE(FP_OPERATOR);
     assert(SIZE == ARRAY_SIZE(OPERATION_NAME));
 
     for (size_t i = 0; i < SIZE; ++i) {
-        int result = FP_OPERATOR[i](m_a, m_b);
+        int result = (this->*FP_OPERATOR[i])();
         cout << OPERATION_NAME[i] << ": " << result << endl;
     }
 }
 
 int main()
 {
-    cout << ADD(1, 2) << endl;
-
     Calculator calc;
     calc.Run();
 }
