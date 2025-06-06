8ebc17029d1573d64f362e79ec9feee7d8d211e6
shunsuke5 <shunsuke1359@gmail.com>
2025-06-06 19:57:43 +0900

    双方向リスト

--- Cpp-Study/main.cpp
+++ Cpp-Study/main.cpp
@@ -1,14 +1,24 @@
-#include "SList.hpp"
+#include "DList.hpp"
 #include <iostream>
 #define ARRAY_SIZE(array) (sizeof (array) / sizeof *(array))
 using namespace std;
 
-typedef SList<int> List;
+typedef DList<int> List;
 typedef List::Node Node;
 
-void Show(const List& list)
+typedef Node* Node::* ShowDirection;
+const ShowDirection SD_FORWARD = &Node::next;
+const ShowDirection SD_BACKWARD = &Node::prev;
+
+/*
+* ShowDirectionを使用することで正順の走査か逆順の走査かを
+* 引数の dir で指定できるようにして、1つの関数にまとめている
+*/
+void Show(const List& list, ShowDirection dir = SD_FORWARD)
 {
-    for (const Node* node = list.GetFirst(); node != NULL; node = node->next) {
+    const Node* eol = list.Eol();
+    const Node* head = eol->*dir;
+    for (const Node* node = head; node != eol; node = node->*dir) {
         cout << node->value << ' ';
     }
     cout << endl;
@@ -18,17 +28,19 @@ int main()
 {
     List list;
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 5; ++i) {
+        list.Push(i);
+    }
+    Show(list);
+    Show(list, SD_BACKWARD);
+
+    for (int i = 5; i < 10; ++i) {
         list.Unshift(i);
     }
-    Show(list);     // 9 8 7 6 5 4 3 2 1 0
+    Show(list);
 
+    list.Pop();
+    list.Pop();
     list.Shift();
-    Show(list);     // 8 7 6 5 4 3 2 1 0
-
-    list.At(3) = 42;
-    Show(list);     // 8 7 6 42 4 3 2 1 0
-
-    list.Clear();
-    Show(list);     // (表示なし)
+    Show(list);
 }
