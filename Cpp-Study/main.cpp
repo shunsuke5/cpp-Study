7bd2fb777f48b9fcd8d9bfdb76a2d9bdef957fef
shunsuke5 <shunsuke1359@gmail.com>
2025-06-08 21:34:28 +0900

    階乗の再帰関数

--- Cpp-Study/main.cpp
+++ Cpp-Study/main.cpp
@@ -4,34 +4,14 @@
 #define ARRAY_SIZE(array) (sizeof (array) / sizeof *(array))
 using namespace std;
 
-class Hoge
+unsigned int Factional(unsigned int num)
 {
-public:
-    explicit Hoge(int n) : m_n(n) {}
-    int Get() const { return m_n; }
-private:
-    int m_n;
-};
+    return num == 0 ? 1 : num * Factional(num - 1);
+}
 
 int main()
 {
-    /*
-    * コピーコンストラクタは明示的に作成しなかった場合、
-    * 暗黙に作られるので以下の一文も問題なく動作する
-    */
-    Stack<Hoge> stack(10);
-
-    try {
-        for (int i = 0; i < 20; ++i) {
-            stack.Push(Hoge(i));
-        }
-    } catch (const overflow_error& e) {
-        cerr << e.what() << endl;
+    for (int i = 0; i < 9; ++i) {
+        cout << i << "! = " << Factional(i) << endl;
     }
-
-    while (!stack.Empty()) {
-        cout << stack.Top().Get() << ' ' << flush;
-        stack.Pop();
-    }
-    cout << endl;
 }
