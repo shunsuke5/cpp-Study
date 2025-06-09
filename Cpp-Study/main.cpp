fbcb8edfe49d54f375335642e651938c8a292418
shunsuke5 <shunsuke1359@gmail.com>
2025-06-09 09:51:42 +0900

    パターンマッチングの再帰関数

--- Cpp-Study/main.cpp
+++ Cpp-Study/main.cpp
@@ -1,17 +1,42 @@
-#include "Stack.hpp"
-#include "Queue.hpp"
 #include <iostream>
+#include <string>
 #define ARRAY_SIZE(array) (sizeof (array) / sizeof *(array))
 using namespace std;
 
-unsigned int Factional(unsigned int num)
+bool Match(const char* str, const char* pat)
 {
-    return num == 0 ? 1 : num * Factional(num - 1);
+    for (; !(*str == '\0' || *pat == '\0'); ++str, ++pat) {
+        if (*pat == '*') {
+            for (++pat; ; ++str) {
+                if (Match(str, pat)) {
+                    return true;
+                }
+
+                if (*str == '\0') {
+                    return false;
+                }
+            }
+        } else {
+            if (*pat != *str) {
+                return false;
+            }
+        }
+    }
+
+    return *pat == *str;
+}
+
+bool Input(string& str)
+{
+    cout << "> " << flush;
+    getline(cin, str);
+    return !(cin.fail() || str == "");
 }
 
 int main()
 {
-    for (int i = 0; i < 9; ++i) {
-        cout << i << "! = " << Factional(i) << endl;
+    string str, pat;
+    while (Input(str) && Input(pat)) {
+        cout << (Match(str.c_str(), pat.c_str()) ? "マッチ" : "NOマッチ") << endl;
     }
 }
